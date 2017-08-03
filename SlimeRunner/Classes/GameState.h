c30a3d96541da4d967ca6a19f9a5f4d098c0ae06
Heejong Cho <hcho16@asu.edu>
2017-08-03 01:31:24 -0700

    annonymous namespace
    tiny change to GAME_STATE enumeration

--- SlimeRunner/Classes/GameState.h
+++ SlimeRunner/Classes/GameState.h
@@ -1,11 +1,12 @@
 #ifndef __GAME_STATE_H__
 #define __GAME_STATE_H__
 
-enum GAME_STATE{
-    PAUSED,
-    PLAYING,
-    OVER
-};
-
+namespace{
+    enum GAME_STATE{
+        PAUSED,
+        PLAYING,
+        OVER
+    };
+}
 
 #endif //__GAME_STATE_H__
